9b5f6094a0bb6a51b8c9393f7c3844f062653d1f
Md Nasim Akhtar <Nasimjam89@gmail.com>
2025-12-23 21:57:26 +0530

    Added Code for Loaclhost:300 issue to production URL LINK

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,5 +4,8 @@ import { getAllUserUrlsDao } from "../dao/user.dao.js"
 export const getAllUserUrls = wrapAsync(async (req, res) => {
     const {_id} = req.user
     const urls = await getAllUserUrlsDao(_id)
-    res.status(200).json({message:"success",urls})
+    const baseUrl = (process.env.APP_URL && process.env.APP_URL.trim()) || `${req.protocol}://${req.get('host')}`
+    const safeBase = baseUrl.replace(/\/$/, '')
+    const urlsWithFull = urls.map(u => ({ ...u.toObject(), shortFullUrl: `${safeBase}/${u.short_url}` }))
+    res.status(200).json({message:"success",urls: urlsWithFull})
 })
